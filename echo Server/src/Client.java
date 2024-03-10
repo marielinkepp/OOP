a8b4b57c2eec833648765ffca8732e65cd98764e
marielinkepp <150765759+marielinkepp@users.noreply.github.com>
2024-03-10 16:13:55 +0200

    Add files via upload

--- echo Server/src/Client.java
+++ echo Server/src/Client.java
@@ -2,26 +2,52 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class Client {
 
     public static void main(String[] args) { // klass Client saab käsurea parameetritega mitu sõnumit (stringi)
 
+        //String[] args = {"file", "shire.txt", "echo", "rivendell", "file", "mordor.jpg"}; // testimiseks
         try (Socket socket = new Socket("localhost", 1337)) {
 
-            // Seejärel saadab client esimese sõnumi, ootab serverilt vastuse (echo) ja prindib selle välja.
-            // Seejärel saadab järgmise sõnumi ja prindib vastuse välja.
-            // Sama korrata iga sõnumiga.
+            //  Client käsurea parameetritega saab määrata, mis requeste ja mis järjekorras saadetakse.
+            //  Käsurea parameetrid käivad paari kaupa: kõigepealt "file" või "echo" ja seejärel faili nimi või sõnumi sisu.
+            //  Käsurida võib sisaldada mitu paari, nt file shire.txt echo rivendell file mordor.jpg.
+            HashMap<String, String> argsMap = getStringStringHashMap(args);
 
             // luuakse DataInputStream ja DataOutputStream: in saab Server klassilt info ja out saadab selle klassile
             DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            out.writeInt(args.length); // serverile saadetakse info stringide arvu kohta
 
-            for (String inputString: args) { // sisendi iteratsioon
-                out.writeUTF(inputString); // iga sisendsõne saadetakse Socketile
-                System.out.println(in.readUTF()); // Socket saadab sõne tagasi ja see väljastatakse ekraanile
-            }
+            out.writeUTF("length:" + argsMap.size()); // Client saadab kõigepealt serverile requestide koguarvu; kõik variable length andmed peaks algama length prefixiga.
+
+            argsMap.forEach((key, value) -> {
+
+                int requestType = 0; // vaikimisi on päringu tüüp 0 (echo)
+
+                // Iga request peaks algama arvuga, mis näitab requesti tüüpi.
+                if (value.equals("file")) requestType = 1; // kui tahetakse failitöötlust, siis on päringu tüüp 1 (file)
+                try {
+                    out.writeUTF(requestType + " " + key); // Socketile saadetakse sõne kujul päringu tüüp + tühik + sõne (failinimi/ekraanile väljastatav sõne)
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    System.out.println(in.readUTF()); // Socket saadab sõne tagasi ja see väljastatakse ekraanile
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
         } catch (IOException e) {throw new RuntimeException(e);}
+    }
+
+    private static HashMap<String, String> getStringStringHashMap(String[] args) {
+        HashMap<String, String> argsMap = new HashMap<>(); // luuakse HashMap, kuhu hakatakse sisendit paarikaupa lisama
+        for (int i = 0; i < args.length; i+=2) { // käsurea isesndi paarikaupa iteratsioon
+            argsMap.put(args[i + 1], args[i]);
+        }
+        return argsMap;
     }
 }
--- echo Server/src/Server.java
+++ echo Server/src/Server.java
@@ -1,8 +1,8 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.List;
 
 public class Server {
 
@@ -10,19 +10,58 @@ public class Server {
 
         int portNumber = 1337; // Klass võtab pordil 1337 ühendusi vastu.
 
-        try (ServerSocket ss = new ServerSocket(portNumber)) { // püütakse luua uut ühendust Server Socketiga
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) { // püütakse luua uut ühendust Server Socketiga
+            System.out.println("Server on port " + portNumber);
 
             while (true) { // tsükkel kestab igavesti ehk server ei sulgu iseenesest
-                try (Socket socket = ss.accept()) { // Server võtab ühenduse vastu
-
-                    // luuakse DataInputStream ja DataOutputStream: in saab Client klassilt sisendi ja out saadab selle klassile tagasi
-                    DataInputStream in = new DataInputStream(socket.getInputStream());
-                    DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
-                    int messageAmount = in.readInt(); // server loeb oodatavate sõnumite arvu
-                    for (int i = 0; i < messageAmount; i++) {out.writeUTF(in.readUTF());} // server loeb sõnumite arv korda sõnumi ja saadab selle tagasi
-                }
+                final Socket clientSocket = serverSocket.accept(); // Server võtab ühenduse vastu
+                new Thread(new MultiThreadSocketManager(clientSocket)).start(); // Iga ühenduse jaoks luuakse uus lõim
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class MultiThreadSocketManager implements Runnable {
+    private Socket clientSocket;
+
+    public MultiThreadSocketManager(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+        // luuakse DataInputStream ja DataOutputStream: in saab Client klassilt sisendi ja out saadab selle klassile tagasi
+        try (DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+             DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream())) {
+
+            int messageAmount = Integer.parseInt(in.readUTF().split("length:")[1]); //Server võtab ühenduse vastu ja loeb requestide arvu (int).
+            for (int i = 0; i < messageAmount; i++) {
+                StringBuilder response = new StringBuilder(); // luuakse uus StringBuilder
+                String[] splitInput = in.readUTF().split(" ");
+                int requestType = Integer.parseInt(splitInput[0]);
+                String requestContent = splitInput[1];
+
+                // Iga response peaks algama arvuga, mis näitab staatust (ok, error): kasutasin siin HTTP response koode (200 OK; 400 Bad Request)
+                // Server peab vastu võtma kahte tüüpi sõnumeid
+                if (requestType == 0) { // kui päringu tüüp on 0 (echo-request), siis sisaldab päringu sisu teksti
+                    response.append("200 ").append(requestContent); // lisatakse kood 200, mis näitab staatust (ok) ja ekraanile väljastatav tekst
+                } else if (requestType == 1) { // kui päringu tüüp on 1 (file-request), siis sisaldab päringu sisu failinime
+
+                    File file = new File(requestContent); // luuakse uus file objekt
+                    if (file.isAbsolute() || !file.exists()) response.append("400: faili ei leitud"); // kui faili nimi on absolute path või faili ei leita, siis lisatakse kood 400, mis näitab staatust (error)
+
+                    else { // muidu tuleb saata vastuseks faili sisu
+                        response.append("200" + "\n"); // lisatakse kood 200, mis näitab staatust (ok)
+                        List<String> lines = Files.readAllLines(file.toPath()); // faili sisu loetakse ridade kaupa massiivi
+                        lines.forEach(line -> response.append(line + "\n")); // iga rida lisatakse koos reavahetusega response sõnele
+                    }
+                }
+                out.writeUTF(response.toString()); // respone saadetakse Client objektile tagasi
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
